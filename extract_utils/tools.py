1ba9781d44a0023d40cb0e41c48a896204902690
Michael Bestas <mikeioannina@gmail.com>
2024-11-06 19:38:47 +0200

    extract_utils: Update lineage_scripts_dir for CalyxOS
    Change-Id: Ide0c6479835d82d7f82bf0f8aa1467795b57149f

--- extract_utils/tools.py
+++ extract_utils/tools.py
@@ -44,7 +44,7 @@ llvm_binaries_dir = path.join(
 llvm_objdump_path = path.join(llvm_binaries_dir, 'llvm-objdump')
 llvm_strip_path = path.join(llvm_binaries_dir, 'llvm-strip')
 
-lineage_scripts_dir = path.join(android_root, 'lineage/scripts')
+lineage_scripts_dir = path.join(android_root, 'calyx/scripts')
 carriersettings_extractor_path = path.join(
     lineage_scripts_dir,
     'carriersettings-extractor/carriersettings_extractor.py',
